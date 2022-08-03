fa020b216fb7abf9eff7fc58bd9bdb17ed3e72fa
RamonPR92 <ramonpr92@hotmail.com>
2022-08-03 12:25:20 -0500

    Introduccion a Angular

---
+++ 01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component, enableProdMode } from '@angular/core';
+
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent{
+    nombre: string = 'Ironman';
+    edad: number = 30;
+
+    obtenerNombre(): string{
+        return `${this.nombre} - ${this.edad}`;
+    }
+
+    get obtenerNombreCapitalizado():string{
+        return `${this.nombre.toUpperCase()}`
+    }
+
+    cambiarNombre(): void{
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 40;
+    }
+}
